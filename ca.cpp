80c9abee2fdaa5f119e82444e4013eb6db635980
som22 <67275652+som22@users.noreply.github.com>
2020-10-01 21:10:56 +0530

    Update ca.cpp

--- ca.cpp
+++ ca.cpp
@@ -1,20 +1,21 @@
 
-#include<iostream.h>
+#include<iostream>
 #include<conio.h>
-
+using namespace std;
  
- void main()
- {
+ int main(){
+ 
  int p;
  float a;
  char ch;
  cout<<"Enter any value "<<endl;
  cin>>p>>a;
- cout<<"choose any operator(+,-,*,/) ::"<<endl;
+ cout<<"choose any operator(+,-,*,/,<,>) ::"<<endl;
  cin>>ch;
 
 
-void switch(ch)
+switch(ch)
+
  {
  case '+':cout<<"addition "<<"="<<p+a<<endl;
 	 break;
@@ -24,8 +25,20 @@ void switch(ch)
  break;
  case '/' : cout<<"division "<<"="<<p/a<<endl;
  break;
+ case '<' : if(p<a){
+	 cout<<p<<" is smaller than  "<<a<<endl;}
+		else{           
+         cout<<p<<" is larger than "<<a<<endl;}
+ break;
+ case '>' : if(p>a){
+	 cout<<a<<" is smaller than  "<<p<<endl;}
+		else{           
+         cout<<a<<" is larger than "<<p<<endl;}
+      
+ break;
 
  default: cout<<"you choose wrong operator "<<endl;
 
  getch();
   }
+return 0;}
